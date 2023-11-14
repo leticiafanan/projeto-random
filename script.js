3585dfdb9113751fa04c11f8dd1fdea18485f76c
leticiafanan <leticiafanan@gmail.com>
2023-11-14 10:51:09 -0300

    alteracao no if do javascript

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".entre").value)
     const max = Math.floor(document.querySelector(".e").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    if(min > max) {
-        alert("O primeiro número deve ser menor que o último")
+
+    if (min >= max) {
+        alert("O valor mínimo tem que ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     alert(result)
